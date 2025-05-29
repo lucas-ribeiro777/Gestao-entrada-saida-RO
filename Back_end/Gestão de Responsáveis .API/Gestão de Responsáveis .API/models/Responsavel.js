c0899313285e2a70b88a0bc8a860483eb5304abc
Caique5445 <caiquegs710@gmail.com>
2025-05-29 15:15:36 -0300

    Delete Back_end/Gestão de Responsáveis .API/Gestão de Responsáveis .API directory

--- Back_end/Gestão de Responsáveis .API/Gestão de Responsáveis .API/models/Responsavel.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Responsavel', {
-    nome_responsavel: DataTypes.STRING,
-    email: DataTypes.STRING,
-    fone: DataTypes.STRING,
-    data_nasc: DataTypes.DATEONLY,
-  });
-};
--- Back_end/Gestão de Responsáveis .API/Gestão de Responsáveis .API/routes/responsavelRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/responsavelController');
-
-router.post('/', controller.criarResponsavel);
-router.get('/', controller.listarResponsaveis);
-router.put('/relacionar', controller.relateResponsavelAluno);
-
-module.exports = router;

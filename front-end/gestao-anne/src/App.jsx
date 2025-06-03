0d4949c47a48f2a4f6cc2ac4f67869bf6baa34f7
joaquimsen2306 <joaquimrafa2021@gmail.com>
2025-06-03 09:27:38 -0300

    att

--- front-end/gestao-anne/src/App.jsx
+++ front-end/gestao-anne/src/App.jsx
@@ -1,37 +1,18 @@
 
-import React from 'react';
 import MenuCadastro from "./components/MenuCadastro/MenuCadastro";
 import Rodape from "./components/Rodape/Rodape";
 import './App.css'
-import CadastroResponsavel from './pages/CadastroResponsavel/CadastroResponsavel';
+import CadastroAluno from './pages/CadastroAluno/CadastroAluno'
+
+
 
 function App() {
   return (
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<CadastroAluno />} />
-        <Route path="/Cadastro-Responsavel" element={<CadastroResponsavel />} />
-        <Route path="/Cadastro-Coordenador" element={<Viloes />} />
-        <Route path="/Cadastro-Professor" element={<Favoritos />} />
-      </Routes>
-    </Router>
+    <>
+      <CadastroAluno/>
+    </>
   );
 
-
-    <div className="container-geral">
-      <MenuCadastro />
-      
-      <main className="conteudo-principal">
-        < CadastroResponsavel />
-      </main>
-
-     <Rodape />
-    </div>
-     
-  );
-  
 }
 
 export default App;

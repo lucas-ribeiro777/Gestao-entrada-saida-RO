f15033bca5d986b5785c01846b9f19ef234014e4
lucas6969-cpu <lucasmoraribeiro@gmail.com>
2025-06-03 08:39:10 -0300

    NAO MEXER NO APP, APENAS NO JSX E CSS QUE VCS ESTÃO FAZENDO

--- front-end/gestao-anne/src/App.jsx
+++ front-end/gestao-anne/src/App.jsx
@@ -1,6 +1,5 @@
 
-import React from 'react';
-import Cabecalho from "./components/Cabecalho/Cabecalho";
+import MenuCadastro from "./components/MenuCadastro/MenuCadastro";
 import Rodape from "./components/Rodape/Rodape";
 import './App.css'
 import CadastroAluno from './pages/CadastroAluno/CadastroAluno'
@@ -9,21 +8,11 @@ import CadastroAluno from './pages/CadastroAluno/CadastroAluno'
 
 function App() {
   return (
-
-    <div className="container-geral">
-      <Cabecalho />
-      
-      <main className="conteudo-principal">
-        {/* Aqui vai o conteúdo principal da página */}
-      </main>
-
-      <Rodape />
-    </div>
-  );
     <>
       <CadastroAluno/>
     </>
-  
+  );
+
 }
 
 export default App;

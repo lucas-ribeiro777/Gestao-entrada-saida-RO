4483706e5332a6c32b296610dd60a74fc3832f61
maaolv <maaolv04@gmail.com>
2025-06-03 15:03:55 -0300

    login

--- front-end/gestao-anne/src/pages/LoginGeral/LoginGeral.jsx
+++ front-end/gestao-anne/src/pages/LoginGeral/LoginGeral.jsx
@@ -1,7 +1,45 @@
 import './LoginGeral.css'
+import Rodape from '../../components/Rodape/Rodape';
+import MenuCadastro from '../../components/MenuCadastro/MenuCadastro';
 
-function LoginGeral() {
-  return <div>Login Geral</div>;
-}
+const LoginGeral= () => {
+  return (
+    <>
+        <MenuCadastro />
+
+        <div className="login-wrapper">
+        <h3 className="login-title">Preencha os dados para fazer login</h3>
+
+        <div className="login-container">
+          <div className="login-row">
+            <input 
+              type="email"
+              placeholder="Digite algo..."
+              className="input-full"
+            />
+          </div>
+
+          <div className="login-row">
+            <input
+              type="password"
+              placeholder="Digite sua senha..."
+              className="input-half"
+            />
+          </div>
+
+          <div className="form-cadastro">
+            <span>
+              Não possui uma conta? <a href="#">Faça seu Cadastro.</a>
+            </span>
+          </div>
+
+          <button className="btn-submit">ENTRAR</button>
+        </div>
+      </div>
+
+      <Rodape />
+    </>
+  );
+};
 
 export default LoginGeral;

ba6469322d56177cb18c99a502b40d33538392ed
maaolv <maaolv04@gmail.com>
2025-06-03 14:20:05 -0300

    componente login
    criação do componente login

--- front-end/gestao-anne/src/App.jsx
+++ front-end/gestao-anne/src/App.jsx
@@ -1,23 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MenuCadastro from './components/MenuCadastro/MenuCadastro';
 import CadastroAluno from './pages/CadastroAluno/CadastroAluno';
 import CadastroDocente from './pages/CadastroDocente/CadastroDocente';
-import CadastroResponsavel from './pages/CadastroResponsavel/CadastroResponsavel';  // corrigido aqui
+import CadastroResponsavel from './pages/CadastroResponsavel/CadastroResponsavel';
 import CadastroCoordenador from './pages/CadastroCoordenador/CadastroCoordenador';
-import Rodape from './components/Rodape/Rodape';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/aluno" element={<CadastroAluno />} />
-          <Route path="/docente" element={<CadastroDocente />} />
-          <Route path="/responsavel" element={<CadastroResponsavel />} />
-          <Route path="/coordenacao" element={<CadastroCoordenador />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/aluno" element={<CadastroAluno />} />
+        <Route path="/docente" element={<CadastroDocente />} />
+        <Route path="/responsavel" element={<CadastroResponsavel />} />
+        <Route path="/coordenacao" element={<CadastroCoordenador />} />
+      </Routes>
+    </Router>
   );
 }
 

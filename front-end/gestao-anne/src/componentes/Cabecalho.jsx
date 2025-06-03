31d2fddc1535d58bed012c71605725aceb7a764d
joaquimsen2306 <joaquimrafa2021@gmail.com>
2025-06-03 08:17:48 -0300

    .

---
+++ front-end/gestao-anne/src/componentes/Cabecalho.jsx
@@ -0,0 +1,12 @@
+<header className="cabecalho">
+  <div className="cabecalho-topo">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/SENAI_logo_2024.png" alt="Logo SENAI" />
+    <h1>Serviço Nacional de Aprendizagem Industrial</h1>
+  </div>
+  <nav className="cabecalho-links">
+    <a href="#" className="ativo">SOU ALUNO</a>
+    <a href="#">SOU DOCENTE</a>
+    <a href="#">SOU RESPONSÁVEL</a>
+    <a href="#">SOU COORDENADOR</a>
+  </nav>
+</header>
---
+++ front-end/gestao-anne/src/componentes/Rodape.jsx
@@ -0,0 +1,13 @@
+<footer className="rodape">
+  <div className="rodape-contato">
+    <strong>DÚVIDAS? ENTRE EM CONTATO!</strong><br />
+    <i className="fab fa-whatsapp" /> (XX) XXXXX-XXXX &nbsp; | &nbsp;
+    <i className="fas fa-phone" /> (XX) XXXX-XXXX
+  </div>
+  <div className="rodape-endereco">
+    <strong>VENHA NOS VISITAR!</strong><br />
+    R. Aristeu Rodrigues Sampaio, 271 – Jardim das Nações,<br />
+    Lençóis Paulista – SP, 18685-730
+  </div>
+</footer>
+import React from 'react';  

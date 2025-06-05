5e7ed6ed78a9ef49924b635fb219b3add7c3f8e9
joaquimsen2306 <joaquimrafa2021@gmail.com>
2025-06-05 10:55:15 -0300

    att responsavel

--- front-end/gestao-anne/src/pages/CadastroResponsavel/CadastroResponsavel.jsx
+++ front-end/gestao-anne/src/pages/CadastroResponsavel/CadastroResponsavel.jsx
@@ -1,80 +1,67 @@
 import React from 'react';
-import './CadastroResponsavel.css'; // estilos específicos do formulário
+import './CadastroResponsavel.css';
 
-// ajuste os caminhos conforme a estrutura do seu projeto
 import MenuCadastro from '../../components/MenuCadastro/MenuCadastro';
 import Rodape from '../../components/Rodape/Rodape';
 
 const CadastroResponsavel = () => {
   return (
     <>
-      {/* Cabeçalho */}
       <MenuCadastro />
 
-      {/* Conteúdo do formulário */}
       <div className="form-wrapper">
         <h3 className="form-title">Preencha os dados para se cadastrar</h3>
 
         <div className="form-container">
           <div className="form-row">
-            {/* Nome completo */}
-            <input
-              type="text"
-              placeholder="Digite algo..."
-              className="input-full"
-            />
+            <div className="form-group">
+              <label htmlFor="nome">Nome Completo</label>
+              <input type="text" id="nome" placeholder="Digite algo..." className="input-full" />
+            </div>
           </div>
 
           <div className="form-row">
-            {/* E-mail */}
-            <input
-              type="email"
-              placeholder="Digite algo..."
-              className="input-full"
-            />
+            <div className="form-group">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" id="email" placeholder="Digite algo..." className="input-full" />
+            </div>
+          </div>
+
+          <div className="form-row form-row-spacing">
+            <div className="form-group">
+              <label htmlFor="dependente">Nome do Dependente</label>
+              <input type="text" id="dependente" placeholder="Digite algo..." className="input-half" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="telefone">Telefone</label>
+              <input type="tel" id="telefone" placeholder="+55 ( )" className="input-half" />
+            </div>
           </div>
 
           <div className="form-row">
-            {/* Nome do dependente */}
-            <input
-              type="text"
-              placeholder="Digite algo..."
-              className="input-half"
-            />
-            {/* Telefone */}
-            <input
-              type="tel"
-              placeholder="+55 ( )"
-              className="input-half"
-            />
-          </div>
-
-          <div className="form-row">
-            {/* Senha */}
-            <input
-              type="password"
-              placeholder="Digite sua senha..."
-              className="input-half"
-            />
-            {/* Confirmar senha */}
-            <input
-              type="password"
-              placeholder="Digite sua senha..."
-              className="input-half"
-            />
+            <div className="form-group">
+              <label htmlFor="senha">Senha</label>
+              <input type="password" id="senha" placeholder="Digite sua senha..." className="input-half" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirmar-senha">Confirmar Senha</label>
+              <input type="password" id="confirmar-senha" placeholder="Digite sua senha..." className="input-half" />
+            </div>
           </div>
 
           <div className="form-login">
             <span>
-              Já possui uma conta? <a href="#">Faça login.</a>
+              Já possui uma conta? <a href="/login">Faça login.</a>
             </span>
           </div>
+        </div>
 
-          <button className="btn-submit">CONCLUIR CADASTRO</button>
+        {/* Botão fora do container azul */}
+        <div className="submit-wrapper">
+          <button className="btn-submit-outside">CONCLUIR CADASTRO</button>
         </div>
       </div>
 
-      {/* Rodapé */}
       <Rodape />
     </>
   );

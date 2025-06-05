96d9ba545617ffb60155ec2d6dcc2a9a4855b4b8
maaolv <maaolv04@gmail.com>
2025-06-05 14:34:13 -0300

    mudancas rodape e login

--- front-end/gestao-anne/src/pages/LoginGeral/LoginGeral.jsx
+++ front-end/gestao-anne/src/pages/LoginGeral/LoginGeral.jsx
@@ -1,9 +1,8 @@
 import './LoginGeral.css';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Importa o hook
+import { useNavigate } from 'react-router-dom';
 import Rodape from '../../components/Rodape/Rodape';
 import MenuCadastro from '../../components/MenuCadastro/MenuCadastro';
-import EsqueciMinhaSenha from '../EsqueciMinhaSenha/EsqueciMinhaSenha';
 
 const LoginGeral = () => {
   const [email, setEmail] = useState('');
@@ -39,38 +38,42 @@ const LoginGeral = () => {
       <MenuCadastro />
 
       <div className="login-wrapper">
-        <h3 className="login-title">Preencha os dados para fazer login</h3>
+  
+  <div className="login-content">
+    <h3 className="login-title">Preencha os dados para fazer login</h3>
 
-        <div className="login-container">
-          <div className="login-row">
-            <label htmlFor="email" className="login-label">E-mail</label>
-            <input id="email" type="email" placeholder="Digite seu e-mail..." className="input-full-login" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-
-          <div className="login-row">
-            <label htmlFor="senha" className="login-label">Senha</label>
-            <input id="senha" type="password" placeholder="Digite sua senha..." className="input-half-login" value={senha} onChange={(e) => setSenha(e.target.value)} />
-            <div className="link-esqueci-senha">
-              <a href="#" onClick={irParaEsqueciSenha}>Esqueci Minha Senha!</a>
-            </div>
-          </div>
-
-          {mensagem && (
-            <div className="mensagem-login">
-              {mensagem}
-            </div>
-          )}
-
-          <div className="form-cadastro">
-            <span>
-              Não possui uma conta?{' '}
-              <a href="#" onClick={irParaCadastro}>Faça seu Cadastro.</a>
-            </span>
-          </div>
-        </div>
-
-        <button className="btn-submit-login" onClick={handleLogin}>ENTRAR</button>
+    <div className="login-container">
+      <div className="login-row">
+        <label htmlFor="email" className="login-label">E-mail</label>
+        <input id="email" type="email" placeholder="Digite seu e-mail..." className="input-full-login" value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
+
+      <div className="login-row">
+        <label htmlFor="senha" className="login-label">Senha</label>
+        <input id="senha" type="password" placeholder="Digite sua senha..." className="input-half-login" value={senha} onChange={(e) => setSenha(e.target.value)} />
+        <div className="link-esqueci-senha">
+          <a href="#" onClick={irParaEsqueciSenha}>Esqueci Minha Senha!</a>
+        </div>
+      </div>
+
+      {mensagem && (
+        <div className="mensagem-login">
+          {mensagem}
+        </div>
+      )}
+
+      <div className="form-cadastro">
+        <span>
+          Não possui uma conta?{' '}
+          <a href="#" onClick={irParaCadastro}>Faça seu Cadastro.</a>
+        </span>
+      </div>
+    </div>
+  </div>
+
+  <button className="btn-submit-login" onClick={handleLogin}>ENTRAR</button>
+</div>
+
 
       <Rodape />
     </>

45881372b8a87bb2ccd25c31fc2d9030e29ae285
ReiOtavio <otaviocomfrio@gmail.com>
2025-06-05 17:09:52 -0300

    inicialalunos

--- front-end/gestao-anne/src/pages/InicialAluno/InicialAluno.jsx
+++ front-end/gestao-anne/src/pages/InicialAluno/InicialAluno.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import "./inicioAluno.css";
+import React from 'react';
+import './InicialAluno.css';
 
 function InicialAluno() {
   return (
     <div className="pagina-inicial">
-      {/* LOGO */}
       <div className="logo-container">
         <img src="./images/LogoSenaiSemAsEscritaDoLado.png" alt="Logo" />
       </div>
 
-      {/* CU */}
       <div className="novo-container">
         <p className="inicio">Início</p>
         <p>Ocorrências</p>
@@ -17,9 +15,7 @@ function InicialAluno() {
         <p>Conta</p>
       </div>
 
-      {/* CAIXAS MODAL SEI LA */}
       <div className="caixas">
-        {/* CAIXA HISTORICO */}
         <div className="caixa">
           <div className="titulo-caixa">HISTÓRICO DO DIA</div>
           <div className="item">07:50 - Entrada Autorizada</div>
@@ -27,7 +23,6 @@ function InicialAluno() {
           <div className="item">09:51 - Saída Autorizada</div>
         </div>
 
-        {/* CAIXA RESPONSAVEIS */}
         <div className="caixa">
           <div className="titulo-caixa">RESPONSÁVEIS DO ALUNO</div>
           <div className="item">Antônio Carlos Marçal</div>
@@ -36,26 +31,27 @@ function InicialAluno() {
         </div>
       </div>
 
-      {/* MAIN */}
-      <main>
-        <h1></h1>
-      </main>
+      <main></main>
 
-      {/* RODAPE */}
       <footer>
         <div className="footer-coluna">
           <p className="titulo-duvidas">DÚVIDAS? ENTRE EM CONTATO!</p>
           <div className="infos">
-            <div className="contato">(XX) XXXXX-XXXX</div>
-            <div className="contato">📞 (XX) XXXX-XXXX</div>
+            <div className="contato">
+              <img src="./images/whatsapp.png" alt="WhatsApp" className="icone-contato" />
+              (11) 4002-8922
+            </div>
+            <div className="contato">
+              <img src="./images/telefone.png" alt="Telefone" className="icone-contato" />
+              (XX) XXXX-XXXX
+            </div>
           </div>
         </div>
+
         <div className="footer-coluna">
           <p className="venha">VENHA NOS VISITAR!</p>
-          <p>
-            <strong>R. Aristeu Rodrigues Sampaio, 271 - Jardim das</strong>
-          </p>
-          <p>Nacoes, Lençóis Paulista - SP, 18685-730</p>
+          <p><strong>R. Aristeu Rodrigues Sampaio, 271 - Jardim das</strong></p>
+          <p>Nações, Lençóis Paulista - SP, 18685-730</p>
         </div>
       </footer>
     </div>

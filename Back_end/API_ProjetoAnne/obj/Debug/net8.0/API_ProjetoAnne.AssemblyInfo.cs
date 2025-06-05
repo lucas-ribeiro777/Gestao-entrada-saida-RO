6344809342ef7b36a0486e895813be1aacb4b2bc
joaquimsen2306 <joaquimrafa2021@gmail.com>
2025-06-05 14:42:17 -0300

    arrumando o formulario para tirar o botao de cadastrar do formulario, ficando igual do wireframe

--- Back_end/API_ProjetoAnne/obj/Debug/net8.0/API_ProjetoAnne.AssemblyInfo.cs
+++ Back_end/API_ProjetoAnne/obj/Debug/net8.0/API_ProjetoAnne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_ProjetoAnne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450edef8ea69e0416a6f12ebfa189d483f4052f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b49fdd03e57c3209b6584010ec7dba48658f875")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_ProjetoAnne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_ProjetoAnne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

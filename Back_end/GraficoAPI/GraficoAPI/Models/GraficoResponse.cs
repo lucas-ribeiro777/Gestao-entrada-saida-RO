5440d1414bab02097f7f509b162e74bcf559b04a
joaquimsen2306 <joaquimrafa2021@gmail.com>
2025-06-17 08:17:02 -0300

    criando a api

---
+++ Back_end/GraficoAPI/GraficoAPI/Models/GraficoResponse.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace GraficoAPI.Models
+{
+    public class GraficosResponse
+    {
+        public List<ControleSaida> ControleSaida { get; set; }
+        public List<MotivoSaida> MotivoSaida { get; set; }
+    }
+}
---
+++ Back_end/GraficoAPI/GraficoAPI/Models/MotivoSaida.cs
@@ -0,0 +1,8 @@
+namespace GraficoAPI.Models
+{
+    public class MotivoSaida
+    {
+        public string Motivo { get; set; }
+        public int Quantidade { get; set; }
+    }
+}

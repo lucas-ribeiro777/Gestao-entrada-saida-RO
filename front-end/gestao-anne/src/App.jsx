42cac5c93f7ad4e4f5a811243f5d5aa6071e0534
maaolv <maaolv04@gmail.com>
2025-06-24 09:18:57 -0300

    criacao ver solicitacao aluno

--- front-end/gestao-anne/src/App.jsx
+++ front-end/gestao-anne/src/App.jsx
@@ -14,6 +14,7 @@ import InicialAluno from './pages/InicialAluno/InicialAluno';
 import InicialProfessor from './pages/InicialProfessor/inicialProfessor';
 import VisualizarSolicitacaoAluno from './pages/VisualizarSolicitacaoAluno/VisualizarSolicitacaoAluno';
 import PesquisarAluno from './pages/PesquisarAlunos/PesquisarAlunos';
+import VerSolicitacaoAluno from './pages/VerSolicitacaoAluno/VerSolicitacaoAluno';
 
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
         <Route path="/InicialProfessor" element={<InicialProfessor />} />
         <Route path="/visualizarsolicitacaoaluno" element={<VisualizarSolicitacaoAluno />} />
         <Route path="/PesquisarAluno" element={<PesquisarAluno />} />
+        <Route path="/VerSolicitacaoAluno" element={<VerSolicitacaoAluno />} />
       </Routes>
     </Router>
   );
